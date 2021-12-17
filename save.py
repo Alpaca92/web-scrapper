a0a15a093cfb49345e9aa316a0ca53277b84cda7
alpaca <zxcvbnm5288@gmail.com>
2021-12-17 13:42:24 +0900

    update route

--- save.py
+++ save.py
@@ -1,8 +1,7 @@
 import csv
 
-
 def save_to_file(jobs):
-    file = open("web-scrapper/jobs.csv", mode="w", encoding="utf-8")
+    file = open("jobs.csv", mode="w", encoding="utf-8")
     writer = csv.writer(file)
     writer.writerow(["title", "company", "location", "link"])
 
